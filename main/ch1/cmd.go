971a1ac1d5c5bcfd84d0eff4c9a6c51ecc86757f
qianz <1685437606@qq.com>
2019-08-08 09:24:11 +0800

    just test my email

--- main/ch1/cmd.go
+++ main/ch1/cmd.go
@@ -7,15 +7,14 @@ import (
 )
 
 type Cmd struct {
-help bool
-version bool
-classPath string
-class string
-args []string
+	help      bool
+	version   bool
+	classPath string
+	class     string
+	args      []string
 }
 
-
-func printUsage()  {
+func printUsage() {
 	// #TODO 2019/7/14 这个help也太简单了吧，基本没啥帮助，写一个更好的help
 	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
 }
@@ -24,12 +23,12 @@ func parseCmd() *Cmd {
 	cmd := Cmd{}
 	flag.Usage = printUsage
 	// #NOTE 2019/7/14 将要注入值的域， 对应的命令行参数，默认值，参数说明
-	flag.BoolVar(&cmd.help,"help",false,"print help message")
-	flag.BoolVar(&cmd.version,"version",false,"print program version")
-	flag.StringVar(&cmd.classPath,"cp","","classpah")
+	flag.BoolVar(&cmd.help, "help", false, "print help message")
+	flag.BoolVar(&cmd.version, "version", false, "print program version")
+	flag.StringVar(&cmd.classPath, "cp", "", "classpah")
 	flag.Parse()
 	args := flag.Args()
-	if len(args) > 0{
+	if len(args) > 0 {
 		cmd.class = args[0]
 		cmd.args = args[1:]
 	}
@@ -37,8 +36,5 @@ func parseCmd() *Cmd {
 	return &cmd
 }
 
-func startJvm(cmd *Cmd)  {
+func startJvm(cmd *Cmd) {
 }
-
-
-
