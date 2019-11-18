8e0c34f89997a5afc4faa53698599f746dc0b835
hugocheng <hugocheng@tencent.com>
2019-11-18 14:17:14 +0800

    add basic usage comment

--- main/ch1/main.go
+++ main/ch1/main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 )
 
+// ./ch1 --version
+// ./ch1 --help
 func main() {
 	cmd := parseCmd()
 	if cmd.version {
 		fmt.Println("version 0.0.1")
-	}else if cmd.help{
+	} else if cmd.help {
 		printUsage()
-	}else {
+	} else {
 		startJvm(cmd)
 	}
 }
-
--- main/ch2/main.go
+++ main/ch2/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
+// ./ch2 -Xjre $JRE_HOME -cp $CLASSPATH java.lang.String
 func main() {
 	cmd := parseCmd()
 	if cmd.version {
 		fmt.Println("version 0.0.1")
-	}else if cmd.help{
+	} else if cmd.help {
 		printUsage()
-	}else {
+	} else {
 		startJvm(cmd)
 	}
 }
-
